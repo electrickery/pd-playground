4f78fd7f4192fa1e672d1cb69dcf5632f5f5792e
Fred Jan Kraan <fjkraan@xs4all.nl>
2016-03-25 21:53:05 +0100

    version 0.2

--- pdParse/changeCoords.tcl
+++ pdParse/changeCoords.tcl
@@ -1,5 +1,7 @@
 #!/usr/bin/env tclsh
 #
+# changeCoords.tcl v0.2
+# fjkraan@xs4all.nl
 
 if {$argc < 4} {
     error "Usage: tclsh changeCoords.tcl patchName xcoord ycoord line1 ?line2? ..."
@@ -30,13 +32,19 @@ for {set start 3} {$lineArgCount > $start} {incr start} {
         }
 }
 
-puts "lines to patch: $lineList"
+#puts "lines not to patch: $lineList"
 
 set lineCount 1
 set f [open $patchName]
 while {[gets $f patchLine] >= 0} {
-    if [regexp {\#[ANX] } $patchLine] {
-        if {[lsearch $lineList $lineCount] >= 0} {
+    if [regexp {\#[AN] } $patchLine] {
+#        puts -nonewline "$lineCount: "
+        puts $patchLine
+        incr lineCount
+        continue
+    }
+    if [regexp {\#[X] } $patchLine] {
+        if {[lsearch $lineList $lineCount] == -1} { # >= 0 for matching lines, == -1 for non-matching lines
             if [regexp {\#X obj (\d+) (\d+) (.+)} $patchLine allOfLine orgX orgY restOfLine] {
 #                puts -nonewline "$lineCount: "
                 puts "#X obj $xcoord $ycoord $restOfLine"
